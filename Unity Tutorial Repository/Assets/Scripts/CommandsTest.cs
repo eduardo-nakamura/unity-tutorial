0007a3e44b59e5544b2a8b6c730d32615f93895c
Eduardo Nakamura <edu-nakamura@hotmail.com>
2023-01-10 18:04:33 -0300

    commands

--- Unity Tutorial Repository/Assets/Scripts/CommandsTest.cs
+++ Unity Tutorial Repository/Assets/Scripts/CommandsTest.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CommandsTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void OnMouseDown() {
+        Debug.Log("Click!");
     }
+    void OnMouseEnter() {
+        Debug.Log("Selected");
+    }
+    // Start is called before the first frame update
+    void Start() { }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Space Key Pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A Key Pressed");
+        }
+
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            Debug.Log("B Key Pressed");
+        }
     }
 }
